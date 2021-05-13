42126162b2e4b062b7865979cb13ab8dab469c50
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-05-13 20:34:04 +0900

    add image-uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_074639) do
+ActiveRecord::Schema.define(version: 2021_05_13_112146) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_074639) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
